e3eefa8685fb3cbaffad659adb80faf5e5247baf
Jorge CALERO <jorge.calero@gmail.com>
2019-10-15 16:01:13 +0200

    Add files via upload

---
+++ public/precache-manifest.1e7ef3701068d98af2afebf2af0c8de0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79cc8ca6b451736f75c64228f29d804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0414cd1bf85943d86ba",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8a9757a4f4bb0fde0e2e",
+    "url": "/static/js/2.685cf6f1.chunk.js"
+  },
+  {
+    "revision": "e0414cd1bf85943d86ba",
+    "url": "/static/js/main.6bb329ae.chunk.js"
+  },
+  {
+    "revision": "96d9506c0ec10f86d92f",
+    "url": "/static/js/runtime-main.a245edba.js"
+  }
+]);
